78a6c7501b4b7f252ef94b71740882eed3ff6e70
chester <chester0148@gmail.com>
2023-02-17 01:00:05 +0800

    refactor: 調整 npm run 加入 環境參數

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,10 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    const NODE_ENV = process.env.NODE_ENV;
+    return {
+      root: true,
+      NODE_ENV,
+    }
   })
 }
