b29e1fde698d42d6503075e21520528083cea190
chester <chester0148@gmail.com>
2023-03-21 06:47:58 +0800

    更新 cheerio  新增一個公用爬蟲

--- services/utils/cheerio.js
+++ services/utils/cheerio.js
@@ -1,5 +1,28 @@
+const request = require('request');
 const cheerio = require('cheerio');
-exports.load = async (html) =>{
-    var result = cheerio.load(html);
-    return result;
+
+exports.load = async (url) => {
+     return new Promise(async (resolve, reject) => { 
+        try {
+                // 發送 HTTP 請求獲取網頁內容
+                request(url, (error, response, html) => {
+                    if (!error && response.statusCode === 200) {
+                        // 用 Cheerio 載入 HTML 內容
+                        const $ = cheerio.load(html);
+
+                        // 篩選出需要的數據
+                        const titles = [];
+                        $('h2.title').each((i, el) => {
+                            titles.push($(el).text());
+                        });
+                       
+                        // 將數據輸出到控制台
+                        // console.log(titles);
+                        return titles;
+                    }
+                });
+        } catch (error) {
+            return reject(error);
+        }
+    })
 }
