86b73942b6a6c40597602cb30655e19c69ddb798
chester <chester0148@gmail.com>
2023-04-04 22:52:49 +0800

    新增 fluent-json-schema  驗證工具

--- routes/BackStage/System.js
+++ routes/BackStage/System.js
@@ -3,11 +3,13 @@ const { getJobSwitch , setJobSwitch } = require('../../services/functions/schedu
 module.exports = async function (fastify, opts) {
 
   fastify.get('/GetScheduleSwitch', async function (request, reply) {
+    const { userInfo } = request;
     return getJobSwitch();
   })
 
   fastify.post('/ChangeScheduleSwitch', async function (request, reply) {
-    const { jobName , status } = request.body;
+    const { jobName, status } = request.body;
+    const { userInfo } = request;
     return setJobSwitch(jobName,status);
   })
 
--- routes/auth/Register.js
+++ routes/auth/Register.js
@@ -1,8 +1,35 @@
+const fluent = require('fluent-json-schema');
 
 module.exports = async function (fastify, opts) {
 
-    fastify.post('/AccountRegister', async function (request, reply) {
-      return 'AccountRegister'
-    })
+  const accountRegisterVerify = {
+    body : fluent.object()
+    .prop('account', fluent.string().minLength(6).maxLength(30).required())
+    .prop('password', fluent.string().minLength(6).maxLength(30).required())
+    .prop('rePassword', fluent.string().minLength(6).maxLength(30).required())
+    .prop('email', fluent.string().minLength(6).maxLength(30).required())
+    .prop('phoneNumber', fluent.string().minLength(6).maxLength(30).required())
+  }
+  
+  fastify.post('/AccountRegister', { schema: accountRegisterVerify}, async function (request, reply){
+    const {
+      account,
+      password,
+      rePassword,
+      email,
+      phoneNumber,
+    } = request.body;
+
+    // if (password != rePassword) {
+      
+    // }
+
+    return {
+      statusCode: "",
+      message: "",
+      data:""
+    }
+  })
+  
 }
     
