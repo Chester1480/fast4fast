9de227152007ea510643734c6da32cf0f328f991
chester <chester0148@gmail.com>
2023-07-07 07:53:34 +0800

    add: 新增 mongodb 辨識用方法

---
+++ services/functions/mongo.js
@@ -0,0 +1,28 @@
+
+
+exports.GetAllCollections = function () { 
+    const map = new Map([
+        [
+            "User",
+            {
+                _id:"",
+                avatar: "",
+                account: "",
+                password: "",
+                name: "",
+                email: "",
+                phone: "",
+                type: "",
+                registeredDate: "",
+                googleOuath2: ""
+            }
+        ],
+        [
+            "",
+            {
+
+            }
+        ]
+
+    ]);
+}
